5b42361df27026e8e5005669cd3c86b0540dccf0
Jose Milton Junior <jmjuniormonteiro@gmail.com>
2022-03-21 14:09:13 -0300

    Adiconando o botão

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // navbar toggler
-let navToggler = document.getElementById('navbar-toggler');
-let navCollapse = document.querySelector('.navbar-collapse');
+let navToggler = document.getElementById("navbar-toggler");
+let navCollapse = document.querySelector(".navbar-collapse");
 
-navToggler.addEventListener('click', () => {
-    navCollapse.classList.toggle('showNav');
+navToggler.addEventListener("click", () => {
+  navCollapse.classList.toggle("showNav");
 });
+
+// Scroll
+const links = document.querySelectorAll('.navbar-nav .nav-item');
+
+for(const link of links){
+    link.addEventListener('click', clickHandler);
+}
+
+function clickHandler(e){
+    e.preventDefault();
+    const href = this.firstElementChild.getAttribute("href");
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
+}
